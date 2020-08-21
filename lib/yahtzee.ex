ad11c7de0f5d4cd6f4e87281adbbd0365bce4928
Laura Cordoba <castelinolaura@gmail.com>
2020-08-21 11:22:19 -0300

    Refactor and implement code review

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -12,9 +12,9 @@ defmodule Yahtzee do
       15
       iex> Yahtzee.score(:yahtzee, [2,2,2,2,2])
       50
-      iex> Yahtzee.score(:sixes, [1,6,2,6,3])
+      iex> Yahtzee.score(6, [1,6,2,6,3])
       12
-      iex> Yahtzee.score(:fives, [1,5,2,5,3])
+      iex> Yahtzee.score(5, [1,5,2,5,3])
       10
       iex> Yahtzee.score(:full_house, [2,2,2,5,5])
       16
@@ -24,59 +24,40 @@ defmodule Yahtzee do
   end
 
   def score(:yahtzee, dice) do
-    len = dice
+    dice
     |> Enum.uniq()
     |> length()
-
-    if len == 1 do 50 else 0 end
+    |> yahtzee_value()
   end
 
-  def score(:sixes, dice) do
-    sum_value_occurrencies(6, dice)
-  end
-
-  def score(:fives, dice) do
-    sum_value_occurrencies(5, dice)
-  end
-
-  def score(:fours, dice) do
-    sum_value_occurrencies(4, dice)
-  end
-
-  def score(:threes, dice) do
-    sum_value_occurrencies(3, dice)
-  end
-
-  def score(:twos, dice) do
-    sum_value_occurrencies(2, dice)
-  end
-
-  def score(:ones, dice) do
-    sum_value_occurrencies(1, dice)
+  def score(pair, dice) when is_integer(pair) and pair <= 6 and pair >= 1 do
+    dice
+    |> Enum.filter(&(&1 == pair))
+    |> Enum.sum()
   end
 
   def score(:pair, dice) do
-    result = group_values(2, dice)
-
-    if length(result) == 1 do sum_group_pairs(result) else 0 end
+    dice
+    |> group_values(2)
+    |> score_pairs(1)
   end
 
   def score(:two_pair, dice) do
-    result = group_values(2, dice)
-
-    if length(result) == 2 do sum_group_pairs(result) else 0 end
+    dice
+    |> group_values(2)
+    |> score_pairs(2)
   end
 
   def score(:three_kind, dice) do
-    result = group_values(3, dice)
-
-    if length(result) == 1 do sum_group_pairs(result) else 0 end
+    dice
+    |> group_values(3)
+    |> score_pairs(1)
   end
 
   def score(:four_kind, dice) do
-    result = group_values(4, dice)
-
-    if length(result) == 1 do sum_group_pairs(result) else 0 end
+    dice
+    |> group_values(4)
+    |> score_pairs(1)
   end
 
   def score(:small_straight, [1, 2, 3, 4, 5]) do
@@ -96,31 +77,49 @@ defmodule Yahtzee do
   end
 
   def score(:full_house, dice) do
-    pair = group_values(2, dice)
-    three_pair = group_values(3, dice)
+    pair = group_values(dice, 2)
+    three_pair = group_values(dice, 3)
 
-    if length(pair) == 1 and length(three_pair) == 1 do sum_group_pairs(pair) + sum_group_pairs(three_pair) else 0 end
+    full_house_calculation(pair, three_pair)
   end
 
-  # Internal Methods
-  def sum_value_occurrencies(value, dice) do
-    dice
-    |> Enum.reject(fn x -> x !== value end)
-    |> Enum.sum()
-  end
-
-  def group_values(lenght_list, dice) do
+  # Private Methods
+  defp group_values(dice, lenght_list) do
     dice
     |> Enum.group_by(&(&1))
     |> Enum.filter(fn {_, list} -> length(list) == lenght_list end)
   end
 
-  def sum_group_pairs([{_x, list} | tail]) do
+  defp sum_group_pairs([{_x, list} | tail]) do
     Enum.sum(list) + sum_group_pairs(tail)
   end
 
-  def sum_group_pairs(list) do
+  defp sum_group_pairs(list) do
     Enum.sum(list)
+  end
+
+  defp yahtzee_value(value) when value == 1 do
+    50
+  end
+
+  defp yahtzee_value(_value) do
+    0
+  end
+
+  defp score_pairs(result, value) when length(result) == value do
+    sum_group_pairs(result)
+  end
+
+  defp score_pairs(_result, _value) do
+    0
+  end
+
+  defp full_house_calculation(pair, three_pair) when length(pair) == 1 and length(three_pair) == 1 do
+    sum_group_pairs(pair) + sum_group_pairs(three_pair)
+  end
+
+  defp full_house_calculation(_pair, _three_pair) do
+    0
   end
 
 end
--- test/yahtzee_test.exs
+++ test/yahtzee_test.exs
@@ -45,51 +45,51 @@ defmodule YahtzeeTest do
 
   describe "placed" do
     test "on sixes match" do
-      assert Yahtzee.score(:sixes, [1, 1, 6, 6, 4]) == 12
+      assert Yahtzee.score(6, [1, 1, 6, 6, 4]) == 12
     end
 
     test "on sixes not match" do
-      assert Yahtzee.score(:sixes, [1, 2, 3, 3, 4]) == 0
+      assert Yahtzee.score(6, [1, 2, 3, 3, 4]) == 0
     end
 
     test "on fives match" do
-      assert Yahtzee.score(:fives, [5, 5, 2, 4, 5]) == 15
+      assert Yahtzee.score(5, [5, 5, 2, 4, 5]) == 15
     end
 
     test "on fives not match" do
-      assert Yahtzee.score(:fives, [1, 2, 2, 4, 3]) == 0
+      assert Yahtzee.score(5, [1, 2, 2, 4, 3]) == 0
     end
 
     test "on fours match" do
-      assert Yahtzee.score(:fours, [1, 1, 2, 3, 4]) == 4
+      assert Yahtzee.score(4, [1, 1, 2, 3, 4]) == 4
     end
 
     test "on fours not match" do
-      assert Yahtzee.score(:fours, [1, 1, 2, 3, 2]) == 0
+      assert Yahtzee.score(4, [1, 1, 2, 3, 2]) == 0
     end
 
     test "on threes match" do
-      assert Yahtzee.score(:threes, [1, 3, 3, 3, 4]) == 9
+      assert Yahtzee.score(3, [1, 3, 3, 3, 4]) == 9
     end
 
     test "on threes not match" do
-      assert Yahtzee.score(:threes, [1, 2, 4, 1, 4]) == 0
+      assert Yahtzee.score(3, [1, 2, 4, 1, 4]) == 0
     end
 
     test "on twos match" do
-      assert Yahtzee.score(:twos, [2, 3, 2, 5, 1]) == 4
+      assert Yahtzee.score(2, [2, 3, 2, 5, 1]) == 4
     end
 
     test "on twos not match" do
-      assert Yahtzee.score(:twos, [1, 3, 6, 5, 1]) == 0
+      assert Yahtzee.score(2, [1, 3, 6, 5, 1]) == 0
     end
 
     test "on ones match" do
-      assert Yahtzee.score(:ones, [3, 3, 1, 4, 5]) == 1
+      assert Yahtzee.score(1, [3, 3, 1, 4, 5]) == 1
     end
 
     test "on ones not match" do
-      assert Yahtzee.score(:ones, [3, 3, 3, 4, 5]) == 0
+      assert Yahtzee.score(1, [3, 3, 3, 4, 5]) == 0
     end
   end
 
